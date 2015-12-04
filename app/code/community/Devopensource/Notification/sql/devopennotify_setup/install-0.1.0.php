36822e33a6673efbfb7891c7914592bbb0a22b2e
Marc Bernabeu <marc.bernabeu@devopensource.com>
2015-12-04 10:43:52 +0100

    fix prefix table

--- app/code/community/Devopensource/Notification/sql/devopennotify_setup/install-0.1.0.php
+++ app/code/community/Devopensource/Notification/sql/devopennotify_setup/install-0.1.0.php
@@ -13,7 +13,7 @@ $installer = $this;
 
 $installer->startSetup();
 
-$table = $installer->getConnection()->newTable('devopen_notification')
+$table = $installer->getConnection()->newTable($installer->getTable('devopennotify/notification'))
     ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
         'unsigned'  => true,
         'nullable'  => false,
